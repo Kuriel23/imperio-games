e79f3c678a25a3e791151dc7b1e0b1bb96c03bc4
ipaddysz <37979213+ipaddysz@users.noreply.github.com>
2021-09-02 01:27:38 +0100

    Add files via upload

---
+++ comandos/moderação/eval.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js')
+const { inspect } = require("util");
+
+module.exports = {
+  name: "eval",
+  description: "",
+  aliases: []
+}
+
+module.exports.run = async (client, message, res) => {
+  if (message.author.id !== "354233941550694400") return message.inlineReply(new Discord.MessageEmbed().setAuthor("» Comando com permissão apenas para desenvolvedor do Bot!", client.err).setColor(client.cor));
+  let code = res.join(" ");
+
+  if (!code) return message.channel.send(new Discord.MessageEmbed().setAuthor("» Digite algum código", client.warn).setColor(client.cor));
+  const user = id => client.users.cache.find(user => user.id == id);
+  const canal = id => client.channels.cache.find(c => c.id == id);
+  const role = id => message.guild.roles.cache.find(r => r.id == id);
+  const ufetch = id => client.users.fetch(r => r.id == id);
+
+  if (message.content.includes("token"))
+    return message.inlineReply(
+      new Discord.MessageEmbed().setAuthor("» [Watch Dogs] ESTE BOT ESTÁ PROTEGIDO COM SISTEMA WATCH DOGS!", client.err).setColor(client.cor)
+    );
+
+  code = code.replace(/^`{3}(js)?|`{3}$/g, "");
+  code = code.replace(/<@!?(\d{16,18})>/g, "user($1)");
+  code = code.replace(/<@!?(\d{16,18})>/g, "ufetch($1)");
+  code = code.replace(/<#?(\d{16,18})>/g, "canal($1)");
+  code = code.replace(/<@&?(\d{16,18})>/g, "role($1)");
+
+  let result;
+
+  try {
+    const evaled = await eval(code);
+    result = inspect(evaled, { depth: 0 });
+  } catch (error) {
+    result = error.toString();
+  }
+  result = result.replace(/_user\((\d{16,18})\)/g, "<@$1>");
+  message.channel.send(result.slice(0, 1030 - 11), { code: "js" });
+};
---
+++ comandos/moderação/manutenção.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "manutenção",
+  description: "",
+  aliases: ["manutencao"]
+}
+
+module.exports.run = async (client, message, res) => {
+  if (message.author.id !== "354233941550694400" && "609935159982161931") return 0;
+  
+  if (client.db2.fetch(`manutencao`).includes("ON")) {
+    client.db2.set(`manutencao`, "OFF");
+    return message.inlineReply(new Discord.MessageEmbed().setAuthor("» Manutenção Desligada!", "https://image.flaticon.com/icons/png/128/889/889731.png").setColor(client.cor))
+  }
+  
+  if (client.db2.fetch(`manutencao`).includes("OFF")) {
+    client.db2.set(`manutencao`, "ON");
+    return message.inlineReply(new Discord.MessageEmbed().setAuthor("» Manutenção Ligada!", "https://image.flaticon.com/icons/png/512/889/889727.png").setColor(client.cor))
+  }
+  
+};
