287f5d451401ad48f943a3986d666d2e8f9fd4d5
Koalin <youtubekilo@outlook.pt>
2021-09-02 01:38:26 +0100

    feat: express

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Olá Zé!'));
+
+app.listen(port, () => console.log(`[Website Iniciado] | https://localhost:${port}`));
 
 // [ - CONSTANTES ]
 
