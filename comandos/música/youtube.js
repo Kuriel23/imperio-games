c1c532aab4eb49c6bb401de8d387170fffccf754
ipaddysz <37979213+ipaddysz@users.noreply.github.com>
2021-09-02 01:28:00 +0100

    Create youtube.js

---
+++ comandos/música/youtube.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "youtube",
+  description: "",
+  aliases: []
+};
+
+module.exports.run = async (client, message, res) => {
+  if (message.member.voice.channel) {
+    client.together
+      .generateTogetherCode(message.member.voice.channelID, "youtube")
+      .then(async invite => {
+        let embed = new Discord.MessageEmbed()
+          .setAuthor(
+            "YOUTUBE",
+            "https://image.flaticon.com/icons/png/512/1384/1384060.png"
+          )
+          .setColor(client.cor)
+          .setDescription("[Clique aqui para começar](" + invite.code + ")");
+        message.inlineReply(embed);
+      });
+  } else {
+    message.inlineReply(
+      new Discord.MessageEmbed()
+        .setAuthor("» Você deveria entrar num canal de voz", client.warn)
+        .setColor(client.cor)
+    );
+  }
+};
